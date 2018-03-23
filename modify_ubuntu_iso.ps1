4ffe30c2005b459a3600cae455f8382c6d9eb463
Another Haos <32611205+anamhaos@users.noreply.github.com>
2018-03-23 06:31:21 -0500

    Add files via upload

---
+++ modify_ubuntu_iso.ps1
@@ -0,0 +1,39 @@
+# paths to media.
+$media = 'C:\Users\bhaos\Desktop\test2'
+$old   = "$media\ubuntu_server.iso"
+$new   = "$media\ubuntu_server-modded.iso"
+
+# paths to tools.
+$tools    = 'C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\oscdimg'
+$oscdimg  = "$tools\oscdimg.exe"
+$etfsboot = "$tools\etfsboot.com"
+$efisys   = "$tools\efisys.bin"
+
+# mount the existing iso.
+$mount = mount-diskimage -imagepath $old -passthru
+
+# get the drive letter assigned to the iso.
+$drive = ($mount | get-volume).driveletter + ':'
+
+# create a temp folder for extracting the existing iso.
+$workspace = "{0}\{1}" -f $env:temp, [system.guid]::newguid().tostring().split('-')[0]
+new-item -type directory -path $workspace
+
+# extract the existing iso to the temporary folder.
+copy-item $drive\* $workspace -force -recurse
+
+# remove the read-only attribtue from the extracted files.
+get-childitem $workspace -recurse | %{ if (! $_.psiscontainer) { $_.isreadonly = $false } }
+
+# pause here... make manual updates to the extracted iso.
+read-host
+
+# create the updated iso.
+$data = '2#p0,e,b"{0}"#pEF,e,b"{1}"' -f $etfsboot, $efisys
+start-process $oscdimg -args @("-bootdata:$data",'-u2','-udfver102', $workspace, $new) -wait -nonewwindow
+
+# remove the extracted content.
+remove-item $workspace -recurse -force
+
+# dismount the iso.
+dismount-diskimage -imagepath $old
